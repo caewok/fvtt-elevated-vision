6fcca7b9c033f53975b219f20d91ce729e40d2ff
Michael Enion <caewok_2000@yahoo.com>
2021-06-17 14:52:44 -0700

    Add return values for wrapped functions.

--- scripts/sightLayer.js
+++ scripts/sightLayer.js
@@ -1,11 +1,28 @@
 import { log } from "./module.js";
 
+ /**
+   * Restrict the visibility of certain canvas assets (like Tokens or DoorControls) based on the visibility polygon
+   * These assets should only be displayed if they are visible given the current player's field of view
+   */
 export function evRestrictVisiblity(wrapped, ...args) {
   const res = wrapped(...args)
   log("evRestrictVisiblity", ...args, res);
+  // no return
 }
 
+ /**
+   * Test whether a point on the Canvas is visible based on the current vision and LOS polygons
+   *
+   * @param {Point} point           The point in space to test, an object with coordinates x and y.
+   * @param {number} tolerance      A numeric radial offset which allows for a non-exact match. For example, if
+   *                                tolerance is 2 then the test will pass if the point is within 2px of a vision
+   *                                polygon.
+   * @param {PIXI.DisplayObject} [object]   An optional reference to the object whose visibility is being tested
+   *
+   * @return {boolean}              Whether the point is currently visible.
+   */
 export function evTestVisibility(wrapped, ...args) {
   const res = wrapped(...args)
   log("evTestVisibility", ...args, res);
+  return res;
 }
--- scripts/wallsLayer.js
+++ scripts/wallsLayer.js
@@ -1,13 +1,29 @@
 import { log } from "./module.js";
 
+/* -------------------------------------------- */
+  /*  Source Polygon Computation                  */
+  /* -------------------------------------------- */
+
+  /**
+   * Compute source polygons of a requested type for a given origin position and maximum radius.
+   * This method returns two polygons, one which is unrestricted by the provided radius, and one that is constrained
+   * by the maximum radius.
+   *
+   * @param {Point} origin            An point with coordinates x and y representing the origin of the test
+   * @param {number} radius           A distance in canvas pixels which reflects the visible range
+   * @param {object} [options={}]     Additional options which modify the sight computation
+   * @param {string} [options.type=sight]     The type of polygon being computed: "movement", "sight", or "sound"
+   * @param {number} [options.angle=360]      An optional limited angle of emission with which to restrict polygons
+   * @param {number} [options.density=6]      The desired radial density of emission for rays, in degrees
+   * @param {number} [options.rotation=0]     The current angle of rotation, used when the angle is limited
+   * @param {boolean} [options.unrestricted=false]  Compute sight that is fully unrestricted by walls
+   *
+   * @returns {{rays: Ray[], los: PIXI.Polygon, fov: PIXI.Polygon}}   The computed rays and polygons
+   */
 export function evComputePolygon(wrapped, ...args) {
   const res = wrapped(...args)
   log("evComputePolygon", ...args, res);
-}
-
-export function evTestWall(wrapped, ...args) {
-  const res = wrapped(...args)
-  log("evTestWall", ...args, res);  
+  return res;
 }
 
  /**
@@ -16,3 +32,9 @@ export function evTestWall(wrapped, ...args) {
    * @param {Wall} wall               The Wall against which to test
    * @return {RayIntersection|null}   A RayIntersection if a collision occurred, or null
    */
+export function evTestWall(wrapped, ...args) {
+  const res = wrapped(...args)
+  log("evTestWall", ...args, res);  
+  return res;
+}
+
