5e461f6d755a0a3b3103977503274111e37cbf6a
Michael Enion <caewok_2000@yahoo.com>
2021-06-20 08:21:41 -0700

    Identify polygon segments and distance from vision point.
    Characterize each segment as near/far. Will then use this information to decide how to shade beyond the segment.

--- scripts/wallsLayer.js
+++ scripts/wallsLayer.js
@@ -42,78 +42,78 @@ Move token 1 square:
 /*
 origin:
 x: 2590.448673605
-​
+
 y: 1705.6773472099756
 
 radius: 630
 options:
   angle: 360
-​
+
   rotation: 30
-​
+
   type: "sight"
-​
+
   unrestricted: false
 
-return: 
-fov: 
+return:
+fov:
 closeStroke: true
-​​
+
 points: Array(188) [ 1963.8998795229877, 1639.8244153513538, 1974.2156851427021, … ]
-​​
+
 radius: 630
-​​
+
 type: 0
-​​
+
 x: 2590.448673605
-​​
+
 y: 1705.6773472099756
 
 los:
 closeStroke: true
-​​
+
 points: Array(188) [ 839.9999999999998, 1521.6977779989038, 840, … ]
-​​
+
 radius: 3101.573720446954
-​​
+
 type: 0
-​​
+
 x: 2590.448673605
-​​
+
 y: 1705.6773472099756
 
 rays (Array):
-​
+
 0: Object { _angle: -3.036872898470133, _distance: 3101.573720446954, y0: 1705.6773472099756, … }
-​​​
+
 A: Object { x: 2590.448673605, y: 1705.6773472099756 }
-​​​
+
 B: Object { x: -494.13430147833196, y: 1381.474612500316 }
-​​​
+
 _angle: -3.036872898470133
-​​​
+
 _c: Object { x: 839.9999999999998, y: 1521.6977779989038, t0: 0.5674830885551752, … }
-​​​
+
 _cs: Map { 55051762 → {…} }
-​​​
+
 _distance: 3101.573720446954
-​​​
+
 dx: -3084.582975083332
-​​​
+
 dy: -324.2027347096596
-​​​
+
 fov: Object { x: 1963.8998795229877, y: 1639.8244153513538 }
-​​​
+
 los: Object { x: 839.9999999999998, y: 1521.6977779989038, t0: 0.5674830885551752, … }
-​​​
+
 slope: 0.10510423526567673
-​​​
+
 x0: 2590.448673605
-​​​
+
 y0: 1705.6773472099756
-​​​
+
 <prototype>: Object { … }
-​​
+
 1: Object { _angle: -2.9321531433504737, _distance: 3101.573720446954, y0: 1705.6773472099756, … } ...
 
 
@@ -129,15 +129,29 @@ y0: 1705.6773472099756
    * @param {number} [options.rotation=0]     The current angle of rotation, used when the angle is limited
    * @param {boolean} [options.unrestricted=false]  Compute sight that is fully unrestricted by walls
 */
-// fov is restricted by radius; lov is not 
+// fov is restricted by radius; lov is not
 
-export function evComputePolygon(wrapped, 
-                                 origin, 
-                                 radius, 
-                                 { type="sight", 
-                                   angle=360, 
-                                   density=6, 
-                                   rotation=0, 
+// https://htmlcolorcodes.com/
+// t# is lesser tints
+const COLORS = {
+  orange: 0xFFA500,
+  oranget1: 0xFFB733,
+  oranget2: 0xFFC55C,
+  yellow: 0xFFFF00,
+  greenyellow: 0xADFF2F,
+  blue: 0x0000FF,
+  red: 0xFF0000,
+  
+  
+}
+
+export function evComputePolygon(wrapped,
+                                 origin,
+                                 radius,
+                                 { type="sight",
+                                   angle=360,
+                                   density=6,
+                                   rotation=0,
                                    unrestricted=false }={}) {
   const res = wrapped(origin, radius, { type: type,
                                         angle: angle,
@@ -149,62 +163,157 @@ export function evComputePolygon(wrapped,
                                         density: density,
                                         rotation: rotation,
                                         unrestricted: unrestricted }, res);
-  
+
   if(type !== "sight") return res;
-  
+
   const isDebuggingVision = FORCE_FOV_DEBUG;
   const debug = isDebuggingVision ? canvas.controls.debug : undefined;
   //const isDebuggingVision = CONFIG.debug.sightRays;
   if(FORCE_FOV_DEBUG || FORCE_TOKEN_VISION_DEBUG) debug.clear();
-  
+
   if(isDebuggingVision) {
-    debug.lineStyle(1, 0xFFFF00).drawShape(res.fov);
-    debug.lineStyle(1, 0xADFF2F).drawShape(res.los);
+    debug.lineStyle(1, COLORS.yellow).drawShape(res.fov);
+    debug.lineStyle(1, COLORS.greenyellow).drawShape(res.los);
   }
-  
+
   /* Plan:
 Cannot easily cutout fov or los with shadows, because it will create holes
-that the PIXI system does not understand.   
-  
+that the PIXI system does not understand.
+
 Instead, build a shadows layer with polygons representing the shadows created
-by terrain or non-infinite walls. 
+by terrain or non-infinite walls.
   */
   const terrain_layer = canvas.layers.filter(l => l?.options?.objectClass?.name === "Terrain")[0];
   if(!terrain_layer) return res;
   let terrains = terrain_layer.placeables; // array of terrains
   if(terrains.length === 0) return res;
+
+  // construct terrain segments, which are used for the filter and elsewhere
+  // only need to do once per terrain
+  // can we do this with forEach?
+  // https://flaviocopes.com/javascript-async-await-array-map/
+  terrains = async() => {
+    return Promise.all(terrains.map(t => {
+      if(!t.document.getFlag(MODULE_ID, "segments")) {
+        await t.document.setFlag(MODULE_ID, "segments", GetPolygonSegments(t.data));
+      }
+      return t;
+    }))
+  }
+
   
   // check if the terrains are within the LOS
   terrains = terrains.filter(t => {
-    for(let i = 0; i < (t.data.points.length - 1); i++) {         
-       const t_segment = new Ray({ x: t.data.x + t.data.points[i][0],
-                                   y: t.data.y + t.data.points[i][1] },
-                                 { x: t.data.x + t.data.points[i + 1][0],
-                                   y: t.data.y + t.data.points[i + 1][1] });
-                                                               
-       if(RayInsidePolygon(t_segment, res.los)) return true;
-       if(RayIntersectsPolygon(t_segment, res.los)) return true;
-    }
-    return false;                           
-  });
+    const segments = t.document.getFlag(MODULE_ID, segments);
   
+    for(let i = 0; i < segments.length; i++) {
+      const segment = segments[i];
+      if(RayInsidePolygon(segments[i], res.los)) return true;          
+      if(RayIntersectsPolygon(segments[i], res.los)) return true;
+    }
+    return false;
+  });
+
   log(`computePolygon found ${terrains.length} terrains.`, terrains);
   
+  // distance types are specific to the origin point and terrain, so keep separate; no flag
+  const terrains_distance_types = terrains.map(t => {
+    const segments = t.document.getFlag(MODULE_ID, segments);
+    const dist_types = CharacterizePolygonDistance(origin, segments);
+    return dist_types;
+  });
+  
   if(isDebuggingVision) {
-    terrains.forEach(t => {
+    terrains.forEach((t, t_idx) => {
+/*
+ Simple version:
+       
       const translated_shape_points = t.shape.points.map((p, idx) => {
         if(idx % 2 === 0) return p + t.data.x; // even, so x
         return p + t.data.y;
       });
+      
       debug.lineStyle(1, 0xFF8C00).drawPolygon(translated_shape_points);
+*/
 
-      //const translated_poly = new PIXI.Polygon(translated_shape_points);
-      //debug.lineStyle(1, 0xFF8C00).drawShape(translated_poly);
+/* To draw from a polygon shape:
+       
+      const translated_poly = new PIXI.Polygon(translated_shape_points);
+      debug.lineStyle(1, 0xFF8C00).drawShape(translated_poly);
+*/
+    // version that draws different-colored sides:
+      const segments = t.document.getFlag(MODULE_ID, segments);
+      const dist_types = terrains_distance_types[t_idx];
+      segments.forEach((s, s_idx) => {
+        const color = dist_types[s_idx] === "far" ? COLORS.orange : COLORS.oranget2;
+        debug.lineStyle(1, color).drawLine(moveTo(s.A.x, s.A.y).lineTo(s.B.x, s.B.y))
+
+      });
     });
   }
-  
-  
+
+  // for each terrain, draw rays from the vision point to terrain points
+  // if Ve < Te, use the closest segments; segment acts as wall to vision
+  // if Ve > Te, use the farthest segments; segment shadows lower elevation beyond
+  // if Ve === Te, can see the terrain at that elevation but nothing lower
+  // closest = ray VT0 and VT1 do not intersect other segments in T
+  // farthest = ray VT0 or VT1 do intersect other segments in T
+
+
   return res;
+}
+
+
+/**
+ * Construct a set of line segments based on polygon points array
+ * @param {Object} Polygon to check, with {x, y, points} where points
+ *    are in [[x0,y0], [x1,y1],...] format.
+ * @return {Array} Array of Rays corresponding to polygon segments.
+ */
+function GetPolygonSegments(poly) {
+  const poly_segments = [];
+  for(let i = 0; i < (poly.points.length - 2); i++) {
+    const poly_segment = { A: { x: poly.x + poly.points[i][0],
+                                y: poly.y + poly.points[i][1] },
+                           B: { x: poly.x + poly.points[i + 1][0],
+                                y: poly.y + poly.points[i + 1][1] }};
+   poly_segments.push(poly_segment);
+  }
+  
+  return poly_segments;
+}
+
+/**
+ * Test if each segment of a polygon is close or far
+ * @param {Point} Vision point to use
+ * @param {Array[Rays]} segments of the polygon, as an Array of Rays
+ * @return [Array] Characterized segments, in order of the points
+ */
+function CharacterizePolygonDistance(vision_point, segments) {
+
+  const segment_type = Array.from('close'.repeat(segments.length));
+  for(let i = 0; i < segments.length; i++) {
+    const ray_A = new Ray(vision_point, segments[i].A);
+    const ray_B = new Ray(vision_point, segments[i].B);
+
+    // if either of the rays intersect any other poly segment, the segment is far
+    for(let j = 0; j < segments.length; j++) {
+      if(i === j) continue; // don't need to test against itself
+      if(ray_A.intersectSegment([segments[i].A.x, segments[i].A.y,
+                                 segments[i].B.x, segments[i].B.y])) {
+        segment_type[i] = "far";
+        break;
+      }
+      
+      if(ray_B.intersectSegment([segments[i].A.x, segments[i].A.y,
+                                 segments[i].B.x, segments[i].B.y])) {
+        segment_type[i] = "far";
+        break;
+      }
+    }
+  }
+  
+  return segment_type;
 }
 
 /*
@@ -255,16 +364,16 @@ function RayInsidePolygon(ray, poly) {
  */
 function RayIntersectsPolygon(ray, poly) {
   // TO-DO: Shortcuts? Sort by closest in some fashion?
-  for(let i = 0; i < (poly.points.length - 1); i++) {
+  for(let i = 0; i < (poly.points.length - 2); i++) {
     const poly_segment = { A: { x: poly.x + poly.points[i][0],
                                 y: poly.y + poly.points[i][1] },
                            B: { x: poly.x + poly.points[i + 1][0],
                                 y: poly.y + poly.points[i + 1][1] }};
-    
+
     if(ray.intersectSegment([poly_segment.A.x, poly_segment.A.y,
                              poly_segment.B.x, poly_segment.B.y])) return true;
   }
-  
+
   return false;
 }
 
@@ -276,7 +385,7 @@ function RayIntersectsPolygon(ray, poly) {
  * @param {Number} base_elevation   Elevation of the ground covered by the shadow.
  * @param {Point} vision_point      Point from which the observer is viewing the wall.
  * @param {Number} vision_elevation Elevation of the observer.
- * @param {Array} ...points         Array of points of the polygon 
+ * @param {Array} ...points         Array of points of the polygon
  */
 class Shadow extends PIXI.Polygon {
   constructor(origin_wall, base_elevation, vision_point, vision_elevation, ...points) {
@@ -295,135 +404,135 @@ class Shadow extends PIXI.Polygon {
    * @param {Wall} wall               The Wall against which to test
    * @return {RayIntersection|null}   A RayIntersection if a collision occurred, or null
    */
-   
+
 /*
 Called *a lot*
 
 Ray:
-​
+
 A: Object { x: 2590.448673605, y: 1750.89734721 }
-​
+
 B: Object { x: -519.0931903894461, y: 2077.723366851742 }
-​
+
 _angle: 3.0368728984701328
-​
+
 _c: Object { x: 840, y: 1934.876916421071, t0: 0.5629281579622838, … }
-​
+
 _cs: Map { 55052175 → {…} }
-​
+
 _distance: 3126.670089895785
-​
+
 dx: -3109.541863994446
-​
+
 dy: 326.82601964174205
-​
+
 fov: Object { x: 1963.8998795229877, y: 1816.7502790686215 }
   x: 1963.8998795229877
-​​
+
  y: 1816.7502790686215
-​
+
 los: Object { x: 840, y: 1934.876916421071, t0: 0.5629281579622838, … }
   t0: 0.5629281579622838
-​​
+
   t1: 0.6137843376879781
-​​
+
   type: 1
-​​
+
   x: 840
-​​
+
   y: 1934.876916421071
-​
+
 slope: -0.10510423526567636
-​
+
 x0: 2590.448673605
-​
+
 y0: 1750.89734721
 
 Wall:
 _bounds: Object { minX: Infinity, minY: Infinity, updateID: -1, … }
-​
+
 _boundsID: 42
-​
+
 _boundsRect: null
-​
+
 _controlled: false
-​
+
 _destroyed: false
-​
+
 _enabledFilters: null
-​
+
 _events: Object {  }
-​
+
 _eventsCount: 0
-​
+
 _hover: false
-​
+
 _lastSortedIndex: 11
-​
+
 _localBounds: null
-​
+
 _localBoundsRect: null
-​
+
 _mask: null
-​
+
 _zIndex: 0
-​
+
 alpha: 1
-​
+
 children: Array [ {…}, {…} ]
-​
+
 controlIcon: null
-​
+
 data: Object { _id: "mRMKbdplB6WeyuTE", move: 1, sense: 1, … }
-- flags: 
-​
+- flags:
+
 directionIcon: null
-​
+
 document: Object { apps: {}, _sheet: null, _object: {…}, … }
-​
+
 doorControl: null
-​
+
 endpoints: Object { _eventsCount: 3, alpha: 1, visible: true, … }
-​
+
 filterArea: null
-​
+
 filters: null
-​
+
 isMask: false
-​
+
 isSprite: false
-​
+
 line: Object { _eventsCount: 2, alpha: 1, visible: true, … }
-​
+
 mouseInteractionManager: Object { state: 0, dragTime: 0, _dragThrottleMS: 17, … }
-​
+
 parent: Object { _eventsCount: 0, alpha: 1, visible: false, … }
-​
+
 renderable: true
-​
+
 roof: undefined
-​
+
 scene: Object { dimensions: {…}, apps: {}, _view: true, … }
-​
+
 sortDirty: true
-​
+
 sortableChildren: false
-​
+
 tempDisplayObjectParent: null
-​
+
 transform: Object { _rotation: 0, _cx: 1, _sx: 0, … }
-​
+
 visible: true
-​
+
 vision: Object { fov: undefined, los: undefined }
-​
+
 worldAlpha: 1
 
 
-*/   
+*/
 export function evTestWall(wrapped, ...args) {
   const res = wrapped(...args)
-  //log("evTestWall", ...args, res);  
+  //log("evTestWall", ...args, res);
   return res;
 }
 
