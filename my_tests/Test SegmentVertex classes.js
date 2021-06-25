11d9d3caf37a4d06670d7220da47b96669a5b915
Michael Enion <caewok_2000@yahoo.com>
2021-06-25 15:26:47 -0700

    Add testing for TerrainPolygon

--- my_tests/Test SegmentVertex classes.js
+++ my_tests/Test SegmentVertex classes.js
@@ -17,4 +17,17 @@ s1.draw(0xFF0000);
 canvas.controls.debug.clear();
 let TerrainPolygon = window["elevated-vision"].TerrainPolygon;
 let p1 = new TerrainPolygon([100, 100, 100, 200, 200, 200, 200, 100, 100, 100]);
+p1.drawPolygon(0xFF0000);
 p1.draw(0xFF0000);
+
+// from terrain
+let terrain_layer = canvas.layers.filter(l => l?.options?.objectClass?.name === "Terrain")[0];
+let terrains = terrain_layer.placeables; // array of terrains
+let terrain_polygons = terrains.map(t => {
+  return TerrainPolygon.fromObject(t.data);
+});
+log(`Transformed ${terrain_polygons.length} terrains`, terrain_polygons);
+
+terrain_polygons.forEach(p => {
+  p.draw(0xFF0000);
+})
