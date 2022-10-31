126a2f1087ffa66bd045a0c1e8e4d16995410c82
Michael Enion <caewok_2000@yahoo.com>
2022-10-31 11:46:28 -0700

    Define epsilon constant

--- scripts/const.js
+++ scripts/const.js
@@ -6,3 +6,6 @@ export const MODULE_ID = "elevatedvision";
 export const WALL_HEIGHT_MODULE_ID = "wall-height";
 export const LEVELS_MODULE_ID = "levels";
 export const FLAG_ELEVATION_IMAGE = "elevationImage";
+
+// Minimum absolute difference of floats before they are considered equal
+export const EPSILON = 1e-08;
