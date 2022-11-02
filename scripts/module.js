00cadfc4de7e376b2c0379ea6f33a2073544ee88
Michael Enion <caewok_2000@yahoo.com>
2022-11-02 16:20:07 -0700

    Remove Wall Height requirement and update pop-up and Readme

--- scripts/module.js
+++ scripts/module.js
@@ -77,15 +77,8 @@ Hooks.once("ready", async function() {
 			title: 'Elevated Vision v0.2.0 Changes!',
 			content: `
 <p>
-As of version 0.2.0, Elevated Vision no longer adjusts token visibility. Thus, by default, tokens in
-shadows (caused by looking down on walls with defined height) will remain visible. Tokens
-otherwise behind walls will be unseen, as expected by default Foundry. Basically, token visibility
-should be equivalent to what you get using the Wall Height module alone; shadows are added but do not
-affect the visibility calculation.
-</p>
-
-<p>
-You can install one or more of the following modules if you need more functionality regarding 3d token visibility:
+As of version 0.2.0, Elevated Vision no longer adjusts token visibility. You can install one or more of the
+following modules if you need more functionality regarding 3d token visibility:
 <ul>
   <li><a href="https://github.com/caewok/fvtt-token-visibility">Alternative Token Visibility</a></li>
   <li><a href="https://github.com/dev7355608/perfect-vision">Perfect Vision</a></li>
@@ -95,6 +88,19 @@ These modules should work together; please report bugs to the relevant git issue
 </p>
 
 <p>
+Elevated Vision also no longer strictly requires, but still very strongly recommends, the <a href="https://foundryvtt.com/packages/wall-height/">Wall Height</a> module.
+With Wall Height, you can set walls and lights to have defined heights. Elevated Vision will create shadows for elevated lights cast on lower walls,
+block lower-elevation lights from illuminating the higher elevation, and create shadows when elevated tokens look down at lower-elevation walls.
+</p>
+<p>
+Thus, with Wall Height but no other token visibility module installed, tokens in shadows
+(caused by looking down on walls with defined height) will remain visible. Tokens otherwise behind walls
+will be unseen, as expected by default Foundry. Basically, token visibility should be equivalent to
+what you get using the Wall Height module alone; shadows are added but do not affect the visibility calculation.
+</p>
+
+<p>
+<br>
 <em>Clicking the button below will make this message no longer display when FoundryVTT loads. If you
 want to keep seeing this message, please click the close button above.</em>
 </p>
