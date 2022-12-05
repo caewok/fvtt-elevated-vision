f7b2b6f38fde368878b98dcced167f63ec1eaa6b
Michael Enion <caewok_2000@yahoo.com>
2022-12-04 16:17:48 -0800

    Remove unused zValue utility function

--- scripts/util.js
+++ scripts/util.js
@@ -168,14 +168,6 @@ export function points3dAlmostEqual(a, b, epsilon = 1e-08) {
 }
 
 /**
- * Convert a grid units value to pixel units, for equivalency with x,y values.
- */
-export function zValue(value) {
-  const { distance, size } = canvas.scene.grid;
-  return (value * size) / distance;
-}
-
-/**
  * Log message only when debug flag is enabled from DevMode module.
  * @param {Object[]} args  Arguments passed to console.log.
  */
