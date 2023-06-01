b9b89c3079fda17c7eac2f194a888836d4a90b88
Michael Enion <caewok_2000@yahoo.com>
2023-05-31 17:20:18 -0700

    Add functionality to convert wall flag data from Wall Height.

--- scripts/walls.js
+++ scripts/walls.js
@@ -10,12 +10,15 @@ import { MODULE_ID, FLAGS } from "./const.js";
 Hooks.on("createWall", createWallHook);
 Hooks.on("updateWall", updateWallHook);
 Hooks.on("deleteWall", deleteWallHook);
+Hooks.on("canvasReady", canvasReadyHook);
 
 function createWallHook(document, options, userId) {
 
 }
 
 function updateWallHook(document, change, options, userId) {
+  // Update Levels data to keep wall elevations in sync.
+
 
 }
 
@@ -23,11 +26,46 @@ function deleteWallHook(document, options, userId) {
 
 }
 
+function canvasReadyHook() {
+  if ( !game.user.isGM ) return;
+
+  importWallHeightWallData(false); // async function
+}
+
+/**
+ * Does this wall need elevation data converted from Wall Height flag to the EV flag?
+ * @param {Wall} wall
+ * @param {boolean} override    If true, this returns true if the Wall Height flag does not
+ *   equal the Elevation flag.
+ * @returns {boolean}
+ */
+function _wallNeedsConversionFromWallHeight(wall, override = false) {
+  if ( !Object.hasOwn(wall.document.flags, "wall-height") ) return false;
+  if ( !override ) return !Boolean(wall.document.flags?.elevatedvision?.elevation);
+
+  const { top, bottom } = wall.document.flags["wall-height"];
+  const ev = wall.document.flags.elevatedvision.elevation;
+  return top !== ev.top && bottom != ev.bottom;
+}
+
+/**
+ * Do one or more walls in the scene need to be updated to be in-sync between
+ * Wall Height and Elevated Vision?
+ * @param {boolean} override    If true, any wall with inconsistent flag data counts.
+ * @returns {boolean}
+ */
+export function wallsNeedConversionFromWallHeight(override = false) {
+  return canvas.walls.some(wall => _wallNeedsConversionFromWallHeight(wall, override));
+}
+
+/**
+ * Import the Wall Height flag data for all the walls in the current scene.
+ * Wall Height flag is "wall-height": { top, bottom}
+ * @param {boolean} override      Should the data override existing Elevated Vision data?
+ * @returns {[Wall]} Output from WallDocument.updateDocuments.
+ */
 export async function importWallHeightWallData(override = false) {
-  const wallsToUpdate = canvas.walls.placeables.filter(wall => {
-    if ( !override && this.document.flags?.elevatedvision?.elevation ) return false;
-    return Object.hasOwn(wall.document.flags, "wall-height");
-  });
+  const wallsToUpdate = canvas.walls.placeables.filter(wall => _wallNeedsConversionFromWallHeight(wall, override));
 
   const updates = wallsToUpdate.map(wall => {
     const top = wall.document.flags["wall-height"]?.top ?? Number.POSITIVE_INFINITY;
@@ -39,3 +77,4 @@ export async function importWallHeightWallData(override = false) {
 
   await WallDocument.updateDocuments(updates, { parent: canvas.scene});
 }
+
