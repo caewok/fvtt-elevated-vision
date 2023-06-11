258402219ae48ddd2ea1a0b84d84046c792dc131
Michael Enion <caewok_2000@yahoo.com>
2023-06-11 07:34:15 -0700

    Temporarily set debug logging to always print

--- scripts/util.js
+++ scripts/util.js
@@ -233,7 +233,8 @@ export function groupBy(list, keyGetter) {
  */
 export function log(...args) {
   try {
-    const isDebugging = game.modules.get("_dev-mode")?.api?.getPackageDebugValue(MODULE_ID);
+    const isDebugging = true;
+    // const isDebugging = game.modules.get("_dev-mode")?.api?.getPackageDebugValue(MODULE_ID);
     if ( isDebugging ) {
       console.log(MODULE_ID, "|", ...args);
     }
