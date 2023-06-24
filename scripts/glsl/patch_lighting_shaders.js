4ce79c9d01638893a9d4e06ce5f599fa8e85bbbd
Michael Enion <caewok_2000@yahoo.com>
2023-06-24 07:04:06 -0700

    Remove the caching of original lighting GLSL code
    Instead, use a boolean switch in the shader to enable/disable lighting.

--- scripts/glsl/patch_lighting_shaders.js
+++ scripts/glsl/patch_lighting_shaders.js
@@ -1,23 +1,15 @@
 /* globals
-canvas,
-PIXI
 */
 "use strict";
 
 import { SETTINGS, getSceneSetting } from "../settings.js";
 import { ShaderPatcher, applyPatches } from "../perfect-vision/shader-patcher.js";
 
-// Add shadow GLSL code to the lighting shaders.
-const originalFragmentSource = new Map();
-
+/**
+ * Wrap AdaptiveLightingShader.create
+ * Add shadow GLSL code to the lighting fragment shaders.
+ */
 export function createAdaptiveLightingShader(wrapped, ...args) {
-//   if ( !originalFragmentSource.has(this.name) ) originalFragmentSource.set(this.name, this.fragmentShader);
-//   const shaderAlgorithm = getSceneSetting(SETTINGS.SHADING.ALGORITHM);
-//   if ( shaderAlgorithm !== SETTINGS.SHADING.TYPES.WEBGL ) {
-//     this.fragmentShader = originalFragmentSource.get(this.name);
-//     return wrapped(...args);
-//   }
-
   applyPatches(this,
     false,
     source => {
@@ -27,21 +19,29 @@ export function createAdaptiveLightingShader(wrapped, ...args) {
   );
 
   const shader = wrapped(...args);
+  const shaderAlgorithm = getSceneSetting(SETTINGS.SHADING.ALGORITHM);
   shader.uniforms.uEVShadowSampler = 0;
+  shader.uniforms.uEVShadows = shaderAlgorithm === SETTINGS.SHADING.TYPES.WEBGL;
   return shader;
 }
 
+/**
+ * Shadow GLSL code to add to the fragment source.
+ */
 function addShadowFragmentCode(source) {
   try {
     source = new ShaderPatcher("frag")
       .setSource(source)
       .addUniform("uEVShadowSampler", "sampler2D")
+      .addUniform("uEVShadows", "bool")
 
       .replace(/gl_FragColor = /, `
-        vec4 EV_shadowTexel = texture2D(uEVShadowSampler, vUvs);
-        float EV_lightAmount = EV_shadowTexel.r;
-        if ( EV_shadowTexel.g < 0.5) EV_lightAmount *= EV_shadowTexel.b;
-        depth *= EV_lightAmount;
+        if ( uEVShadows ) {
+          vec4 EV_shadowTexel = texture2D(uEVShadowSampler, vUvs);
+          float EV_lightAmount = EV_shadowTexel.r;
+          if ( EV_shadowTexel.g < 0.5) EV_lightAmount *= EV_shadowTexel.b;
+          depth *= EV_lightAmount;
+        }
         gl_FragColor =`)
 
       .getSource();
