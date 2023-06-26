509b1ba0a13e703b7def193605bea570273caaa1
Michael Enion <caewok_2000@yahoo.com>
2023-06-26 09:29:52 -0700

    Add renderer that covers the entire canvas.

--- scripts/glsl/ShadowTextureRenderer.js
+++ scripts/glsl/ShadowTextureRenderer.js
@@ -12,10 +12,7 @@ export class ShadowTextureRenderer {
   // TODO: Allow to be changed via CONFIG.
 
   /** @type {number} */
-  static #MAX_WIDTH = 4096;
-
-  /** @type {number} */
-  static #MAX_HEIGHT = 4096;
+  static MAX_TEXTURE_SIZE = 4096;
 
   /** @type PIXI.RenderTexture */
   renderTexture;
@@ -37,7 +34,7 @@ export class ShadowTextureRenderer {
    */
   get width() {
     const diameter = this.source.radius * 2;
-    return Math.min(this.constructor.#MAX_WIDTH, diameter);
+    return Math.min(ShadowTextureRenderer.MAX_TEXTURE_SIZE, diameter);
   }
 
   /**
@@ -46,7 +43,7 @@ export class ShadowTextureRenderer {
    */
   get height() {
     const diameter = this.source.radius * 2;
-    return Math.min(this.constructor.#MAX_HEIGHT, diameter);
+    return Math.min(ShadowTextureRenderer.MAX_TEXTURE_SIZE, diameter);
   }
 
   /**
@@ -101,6 +98,44 @@ export class ShadowTextureRenderer {
    */
   destroy() {
     this.renderTexture.destroy();
+  }
+}
+
+export class ShadowVisionLOSTextureRenderer extends ShadowTextureRenderer {
+  get width() {
+    const { width, height } = canvas.dimensions;
+    let texWidth = width;
+    const maxSize = ShadowTextureRenderer.MAX_TEXTURE_SIZE;
+
+    if ( width > height && width > maxSize ) texWidth = maxSize;
+    else if ( height > width && height > maxSize ) {
+      const reduction = maxSize / height;
+      texWidth = width * reduction;
+    }
+    return texWidth;
+  }
+
+  get height() {
+    const { width, height } = canvas.dimensions;
+    let texHeight = height;
+    const maxSize = ShadowTextureRenderer.MAX_TEXTURE_SIZE;
+
+    if ( width > height && width > maxSize ) {
+      const reduction = maxSize / width;
+      texHeight = height * reduction;
+    } else if ( height > width && height > maxSize ) texHeight = maxSize;
+
+    return texHeight;
+  }
+
+  /**
+   * Source bounds defined by the canvas rectangle or scene rectangle.
+   * @type {PIXI.Rectangle}
+   */
+  get sourceBounds() {
+//     const { rect, sceneRect } = canvas.dimensions;
+//     if ( sceneRect.contains(this.source.object.center) ) return sceneRect;
+    return canvas.dimensions.rect;
   }
 }
 
