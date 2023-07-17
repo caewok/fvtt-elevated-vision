cef1e1b90bd5111893706a3cafd3d094e330b6ac
Michael Enion <caewok_2000@yahoo.com>
2023-07-17 06:50:53 -0700

    Wall file

---
+++ scripts/Wall.js
@@ -0,0 +1,87 @@
+/* globals
+canvas,
+flattenObject
+*/
+"use strict";
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+
+import { SourceShadowWallGeometry } from "./glsl/SourceShadowWallGeometry.js";
+
+// Methods related to Wall
+
+export const PATCHES = {};
+PATCHES.WEBGL = {};
+
+// NOTE: Wall Document Hooks
+
+/**
+ * A hook event that fires for every embedded Document type after conclusion of a creation workflow.
+ * Substitute the Document name in the hook event to target a specific type, for example "createToken".
+ * This hook fires for all connected clients after the creation has been processed.
+ *
+ * @event createDocument
+ * @category Document
+ * @param {Document} document                       The new Document instance which has been created
+ * @param {DocumentModificationContext} options     Additional options which modified the creation request
+ * @param {string} userId                           The ID of the User who triggered the creation workflow
+ */
+function createWall(wallD, _options, _userId) {
+  const sources = [
+    ...canvas.effects.lightSources,
+    ...canvas.tokens.placeables.map(t => t.vision)
+  ];
+
+  for ( const src of sources ) src.wallAdded(wallD.object);
+}
+
+/**
+ * A hook event that fires for every Document type after conclusion of an update workflow.
+ * Substitute the Document name in the hook event to target a specific Document type, for example "updateActor".
+ * This hook fires for all connected clients after the update has been processed.
+ *
+ * @event updateDocument
+ * @category Document
+ * @param {Document} document                       The existing Document which was updated
+ * @param {object} change                           Differential data that was used to update the document
+ * @param {DocumentModificationContext} options     Additional options which modified the update request
+ * @param {string} userId                           The ID of the User who triggered the update workflow
+ */
+function updateWall(wallD, data, _options, _userId) {
+  const changes = new Set(Object.keys(flattenObject(data)));
+  // TODO: Will eventually need to monitor changes for sounds and sight, possibly move.
+  // TODO: Need to deal with threshold as well
+  if ( !(SourceShadowWallGeometry.CHANGE_FLAGS.some(f => changes.has(f))) ) return;
+
+  const sources = [
+    ...canvas.effects.lightSources,
+    ...canvas.tokens.placeables.map(t => t.vision)
+  ];
+
+  for ( const src of sources ) src.wallUpdated(wallD.object, changes);
+}
+
+/**
+ * A hook event that fires for every Document type after conclusion of an deletion workflow.
+ * Substitute the Document name in the hook event to target a specific Document type, for example "deleteActor".
+ * This hook fires for all connected clients after the deletion has been processed.
+ *
+ * @event deleteDocument
+ * @category Document
+ * @param {Document} document                       The existing Document which was deleted
+ * @param {DocumentModificationContext} options     Additional options which modified the deletion request
+ * @param {string} userId                           The ID of the User who triggered the deletion workflow
+ */
+function deleteWall(wallD, _options, _userId) {
+  const sources = [
+    ...canvas.effects.lightSources,
+    ...canvas.tokens.placeables.map(t => t.vision)
+  ];
+
+  for ( const src of sources ) src.wallRemoved(wallD.id);
+}
+
+PATCHES.WEBGL.HOOKS = {
+  createWall,
+  updateWall,
+  deleteWall
+};
