f10733cda910ec984cb36b110c1243ecfff43733
Michael Enion <caewok_2000@yahoo.com>
2023-07-17 06:52:24 -0700

    PIXI.Graphics file

---
+++ scripts/PIXI_Graphics.js
@@ -0,0 +1,35 @@
+/* globals
+ClockwiseSweepPolygon
+*/
+"use strict";
+
+import { getSceneSetting, SETTINGS } from "./settings.js";
+
+const PATCHES = {};
+PATCHES.POLYGONS = {};
+
+/**
+ * Wrap PIXI.Graphics.drawShape.
+ * If passed a polygon with an array of polygons property, use that to draw with holes.
+ */
+function drawShape(wrapped, shape) {
+  if ( !(shape instanceof ClockwiseSweepPolygon) ) return wrapped(shape);
+
+  const { ALGORITHM, TYPES } = SETTINGS.SHADING;
+  const shaderAlgorithm = getSceneSetting(ALGORITHM) ?? TYPES.NONE;
+  if ( (shaderAlgorithm === TYPES.POLYGONS || shaderAlgorithm === TYPES.WEBGL) && Object.hasOwn(shape, "_evPolygons") ) {
+    for ( const poly of shape._evPolygons ) {
+      if ( poly.isHole ) {
+        this.beginHole();
+        this.drawShape(poly);
+        this.endHole();
+      } else this.drawShape(poly);
+    }
+  } else {
+    return wrapped(shape);
+  }
+
+  return this;
+}
+
+PATCHES.POLYGONS.WRAPS = { drawShape };
