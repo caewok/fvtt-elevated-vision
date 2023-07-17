738b19a2161d58b40eaa88ffaa0b8a6558c7ad9a
Michael Enion <caewok_2000@yahoo.com>
2023-07-17 06:41:42 -0700

    Create Canvas file

---
+++ scripts/Canvas.js
@@ -0,0 +1,18 @@
+/* globals
+canvas
+*/
+"use strict";
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+
+// Methods related to Canvas
+
+/**
+ * Track mouse events for the canvas elevation layer.
+ */
+function _onMouseMove(wrapper, event) {
+  wrapper(event);
+  canvas.elevation._onMouseMove(event);
+}
+
+export const PATCHES = {};
+PATCHES.BASIC = { WRAPS: { _onMouseMove } };
