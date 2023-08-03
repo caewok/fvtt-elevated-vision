1c448a32c40d410a3b8ff8c61fed1db30e159185
Michael Enion <caewok_2000@yahoo.com>
2023-08-03 16:40:39 -0700

    Add Patcher class used in walled templates

---
+++ scripts/Patcher.js
@@ -0,0 +1,298 @@
+/* globals
+CONFIG,
+Hooks,
+libWrapper
+*/
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+"use strict";
+
+import { MODULE_ID } from "./const.js";
+
+// Class to control patching: libWrapper, hooks, added methods.
+export class Patcher {
+  /**
+   * @typedef {object} RegTracker
+   * @property {Map<number, object[]} PATCHES   libWrapper patch ids --> arguments to libWrapper
+   * @property {Map<string, object[]} METHODS   class method names --> arguments to add the method
+   * @property {Map<number, object[]} HOOKS     hook number --> arguments to the hook
+   * @property {function} regHook               decorated function to register hooks for the group
+   * @property {function} regMethod             decorated function to register methods for the group
+   * @property {function} regWrap               decorated function to register wraps for the group
+   * @property {function} regOverride           decorated function to register overrides for the group
+   */
+
+  /** @type {RegTracker} */
+  regTracker = {};
+
+  /** @type {Set} */
+  groupings = new Set();
+
+  /** @type {object} */
+  patches;
+
+  constructor(patches) {
+    this.patches = patches;
+    this.#initializeRegistrationTracker();
+  }
+
+  groupIsRegistered(groupName) {
+    const regObj = this.regTracker[groupName];
+    return regObj.PATCHES.size || regObj.METHODS.size || regObj.HOOKS.size;
+  }
+
+  /**
+   * Run through the patches and construct mappings for each group in the RegTracker.
+   */
+  #initializeRegistrationTracker() {
+    // Decorate each group type and create one per option.
+    this.groupings.clear();
+    Object.values(this.patches).forEach(obj => Object.keys(obj).forEach(k => this.initializeGroup(k)));
+  }
+
+  /**
+   * Register a specific group in the tracker.
+   * @param {string} groupName
+   */
+  initializeGroup(groupName) {
+    if ( this.groupings.has(groupName) ) return;
+    this.groupings.add(groupName);
+    const regObj = this.regTracker[groupName] = {};
+    regObj.PATCHES = new Map();
+    regObj.METHODS = new Map();
+    regObj.HOOKS = new Map();
+    regObj.regWrap = regDec(wrap, regObj.PATCHES);
+    regObj.regOverride = regDec(override, regObj.PATCHES);
+    regObj.regMethod = regDec(this.constructor.addClassMethod, regObj.METHODS);
+    regObj.regHook = regDec(addHook, regObj.HOOKS);
+  }
+
+  /**
+   * Register all of a given group of patches.
+   */
+  registerGroup(groupName) {
+    for ( const className of Object.keys(this.patches) ) this._registerGroupForClass(className, groupName);
+  }
+
+  /**
+   * For a given group of patches, register all of them.
+   */
+  _registerGroupForClass(className, groupName) {
+    const grp = this.patches[className][groupName];
+    if ( !grp ) return;
+    for ( const [key, obj] of Object.entries(grp) ) {
+      const prototype = !key.includes("STATIC");
+      let override = false;
+      let getter = false;
+      switch ( key ) {
+        case "HOOKS": this._registerHooks(obj, groupName); break;
+        case "STATIC_OVERRIDES":
+        case "OVERRIDES":
+          override = true;
+        case "STATIC_WRAPS": // eslint-disable-line no-fallthrough
+        case "WRAPS":
+          this._registerWraps(obj, groupName, className, { override, prototype });
+          break;
+        case "STATIC_GETTERS":
+        case "GETTERS":
+          getter = true;
+        default:  // eslint-disable-line no-fallthrough
+          this._registerMethods(obj, groupName, className, { prototype, getter });
+      }
+    }
+  }
+
+  /**
+   * Register a group of methods in libWrapper.
+   * @param {object|Map<string, function>} wraps      The functions to register
+   * @param {string} groupName                        Group to use for the tracker
+   * @param {string} className                        The class name to use; will be checked against CONFIG
+   * @param {object} [opt]                            Options passed to libWrapper
+   * @param {boolean} [opt.prototype]                 Whether to use class.prototype or just class
+   * @param {boolean} [opt.override]                  If true, use override in libWrapper
+   * @param {libWrapper.PERF_FAST|PERF_AUTO|PERF_NORMAL}
+   */
+  _registerWraps(wraps, groupName, className,
+    { prototype = true, override = false, perf_mode = libWrapper.PERF_FAST } = {}) {
+    className = this.constructor.lookupByClassName(className, { returnPathString: true });
+    if ( prototype ) className = `${className}.prototype`;
+    const wrapFn = override ? "regOverride" : "regWrap";
+    for ( const [name, fn] of Object.entries(wraps) ) {
+      const methodName = `${className}.${name}`;
+      this.regTracker[groupName][wrapFn](methodName, fn, { perf_mode });
+    }
+  }
+
+  /**
+   * Register a group of new methods.
+   * @param {object|Map<string, function>} methods    The functions to register
+   * @param {string} groupName                        Group to use for the tracker
+   * @param {string} className                        The class name to use; will be checked against CONFIG
+   * @param {object} [opt]                            Options passed to teh registration
+   * @param {boolean} [opt.prototype]                 Whether to use class.prototype or just class
+   * @param {boolean} [opt.getter]                    If true, register as a getter
+   */
+  _registerMethods(methods, groupName, className, { prototype = true, getter = false } = {}) {
+    let cl = this.constructor.lookupByClassName(className);
+    if ( prototype ) cl = cl.prototype;
+    for ( const [name, fn] of Object.entries(methods) ) {
+      this.regTracker[groupName].regMethod(cl, name, fn, { getter });
+    }
+  }
+
+  /**
+   * Register a group of hooks.
+   * @param {object|Map<string, function>} methods    The hooks to register
+   * @param {string} groupName                        Group to use for the tracker
+   */
+  _registerHooks(hooks, groupName) {
+    for ( const [name, fn] of Object.entries(hooks) ) {
+      this.regTracker[groupName].regHook(name, fn);
+    }
+  }
+
+  /**
+   * Deregister an entire group of patches.
+   * @param {string} groupName    Name of the group to deregister.
+   */
+  deregisterGroup(groupName) {
+    const regObj = this.regTracker[groupName];
+    this.#deregisterPatches(regObj.PATCHES);
+    this.#deregisterMethods(regObj.METHODS);
+    this.#deregisterHooks(regObj.HOOKS);
+  }
+
+  /**
+   * Deregister all libWrapper patches in this map.
+   */
+  #deregisterPatches(map) {
+    map.forEach((_args, id) => libWrapper.unregister(MODULE_ID, id, false));
+    map.clear();
+  }
+
+  /**
+   * Deregister all hooks in this map.
+   */
+  #deregisterHooks(map) {
+    map.forEach((hookName, id) => Hooks.off(hookName, id));
+    map.clear();
+  }
+
+  /**
+   * Deregister all methods in this map.
+   */
+  #deregisterMethods(map) {
+    map.forEach((args, _id) => {
+      const { cl, name } = args;
+      delete cl[name];
+    });
+    map.clear();
+  }
+
+  /**
+   * Add a method or a getter to a class.
+   * @param {class} cl      Either Class.prototype or Class
+   * @param {string} name   Name of the method
+   * @param {function} fn   Function to use for the method
+   * @param {object} [opts] Optional parameters
+   * @param {boolean} [opts.getter]     True if the property should be made a getter.
+   * @param {boolean} [opts.optional]   True if the getter should not be set if it already exists.
+   * @returns {undefined|object<id{string}} Either undefined if the getter already exists or the cl.prototype.name.
+   */
+  static addClassMethod(cl, name, fn, { getter = false, optional = false } = {}) {
+    if ( optional && Object.hasOwn(cl, name) ) return undefined;
+    const descriptor = { configurable: true };
+    if ( getter ) descriptor.get = fn;
+    else {
+      descriptor.writable = true;
+      descriptor.value = fn;
+    }
+    Object.defineProperty(cl, name, descriptor);
+
+    const prototypeName = cl.constructor?.name;
+    const id = `${prototypeName ?? cl.name }.${prototypeName ? "prototype." : ""}${name}`; // eslint-disable-line template-curly-spacing
+    return { id, args: { cl, name } };
+  }
+
+  /**
+   * A thorough lookup method to locate Foundry classes by name.
+   * Relies on CONFIG where possible, falling back on eval otherwise.
+   * @param {string} className
+   * @param {object} [opts]
+   * @param {boolean} [opts.returnPathString]   Return a string path to the object, for libWrapper.
+   * @returns {class}
+   */
+  static lookupByClassName(className, { returnPathString = false } = {}) {
+    let isDoc = className.endsWith("Document");
+    let isConfig = className.endsWith("Config");
+    let baseClass = isDoc ? className.replace("Document", "") : isConfig ? className.replace("Config", "") : className;
+
+    const configObj = CONFIG[baseClass];
+    if ( !configObj || isConfig ) return returnPathString ? className : eval?.(`"use strict";(${className})`);
+
+    // Do this the hard way to catch inconsistencies
+    switch ( className ) {
+      case "Actor":
+      case "ActiveEffect":
+      case "Item":
+        isDoc = true; break;
+    }
+
+    if ( isDoc && configObj.documentClass ) {
+      return returnPathString ? `CONFIG.${baseClass}.documentClass` : configObj.documentClass;
+    }
+
+    if ( configObj.objectClass ) return returnPathString ? `CONFIG.${baseClass}.objectClass` : configObj.objectClass;
+    return returnPathString ? className : eval?.(`"use strict";(${className})`);
+  }
+
+}
+
+// ----- NOTE: Helper functions ----- //
+
+/**
+ * Helper to wrap/mix/override methods.
+ * @param {string} method       Method to wrap
+ * @param {function} fn         Function to use for the wrap
+ * @param {object} [options]    Options passed to libWrapper.register. E.g., { perf_mode: libWrapper.PERF_FAST}
+ * @returns {object<id{number}, args{string}} libWrapper ID and the method used
+ */
+function wrap(method, fn, options = {}) {
+  const id = libWrapper.register(MODULE_ID, method, fn, libWrapper.WRAPPER, options);
+  return { id, args: method }
+}
+
+// Currently unused
+// function mixed(method, fn, options = {}) {
+//   const id = libWrapper.register(MODULE_ID, method, fn, libWrapper.MIXED, options);
+//   return { id, args: method }
+// }
+
+function override(method, fn, options = {}) {
+  const id = libWrapper.register(MODULE_ID, method, fn, libWrapper.OVERRIDE, options)
+  return { id, args: method }
+}
+
+/**
+ * Wrapper to add a hook, b/c calling Hooks.on directly with a decorator does not work.
+ * @param {string} hookName     Name of the hook
+ * @param {function} fn         Function to use for the hook
+ * @returns {object<id{number}, args{string}} hook id and the hook name
+ */
+function addHook(hookName, hookFn) {
+  const id = Hooks.on(hookName, hookFn);
+  return { id, args: hookName };
+}
+
+/**
+ * Decorator to register and record a patch, method, or hook.
+ * @param {function} fn   A registration function that returns an id. E.g., libWrapper or Hooks.on.
+ * @param {Map} map       The map in which to store the id along with the arguments used when registering.
+ * @returns {number} The id
+ */
+function regDec(fn, map) {
+  return function() {
+    const { id, args } = fn.apply(this, arguments);
+    map.set(id, args);
+    return id;
+  };
+}
--- scripts/patching.js
+++ scripts/patching.js
@@ -1,16 +1,13 @@
 /* globals
 canvas,
-CONFIG,
 GlobalLightSource,
-Hooks,
-libWrapper
 */
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
 
 // Patches
 
-import { MODULE_ID } from "./const.js";
+import { Patcher } from "./Patcher.js";
 import { getSetting, getSceneSetting, SETTINGS } from "./settings.js";
 import { DirectionalLightSource } from "./DirectionalLightSource.js";
 
@@ -80,218 +77,10 @@ export const PATCHES = {
   Wall: PATCHES_Wall
 };
 
-
-/**
- * Helper to wrap methods.
- * @param {string} method       Method to wrap
- * @param {function} fn         Function to use for the wrap
- * @param {object} [options]    Options passed to libWrapper.register. E.g., { perf_mode: libWrapper.PERF_FAST}
- * @returns {number} libWrapper ID
- */
-function wrap(method, fn, options = {}) {
-  return libWrapper.register(MODULE_ID, method, fn, libWrapper.WRAPPER, options);
-}
-
-// Currently unused
-// function mixed(method, fn, options = {}) {
-//   return libWrapper.register(MODULE_ID, method, fn, libWrapper.MIXED, options);
-// }
-
-function override(method, fn, options = {}) {
-  return libWrapper.register(MODULE_ID, method, fn, libWrapper.OVERRIDE, options);
-}
-
-/**
- * Helper to add a method or a getter to a class.
- * @param {class} cl      Either Class.prototype or Class
- * @param {string} name   Name of the method
- * @param {function} fn   Function to use for the method
- * @param {object} [opts] Optional parameters
- * @param {boolean} [opts.getter]     True if the property should be made a getter.
- * @param {boolean} [opts.optional]   True if the getter should not be set if it already exists.
- * @returns {undefined|string} Either undefined if the getter already exists or the cl.prototype.name.
- */
-function addClassMethod(cl, name, fn, { getter = false, optional = false } = {}) {
-  if ( optional && Object.hasOwn(cl, name) ) return undefined;
-  const descriptor = { configurable: true };
-  if ( getter ) descriptor.get = fn;
-  else {
-    descriptor.writable = true;
-    descriptor.value = fn;
-  }
-  Object.defineProperty(cl, name, descriptor);
-
-  const prototypeName = cl.constructor?.name;
-  return `${prototypeName ?? cl.name }.${prototypeName ? "prototype." : ""}${name}`; // eslint-disable-line template-curly-spacing
-}
-
-
-/**
- * Wrapper to add a hook, b/c calling Hooks.on directly with a decorator does not work.
- */
-function addHook(hookName, hookFn) { return Hooks.on(hookName, hookFn); }
-
-// ----- NOTE: Track libWrapper patches, method additions, and hooks ----- //
-/**
- * Decorator to register and record a patch, method, or hook.
- * @param {function} fn   A registration function that returns an id. E.g., libWrapper or Hooks.on.
- * @param {Map} map       The map in which to store the id along with the arguments used when registering.
- * @returns {number} The id
- */
-function regDec(fn, map) {
-  return function() {
-    const id = fn.apply(this, arguments);
-    map.set(id, arguments);
-    return id;
-  };
-}
-
-/**
- * Deregister shading wrappers.
- * Used when switching shadow algorithms. Deregister all, then re-register needed wrappers.
- */
-function deregisterPatches(map) {
-  map.forEach((_args, id) => libWrapper.unregister(MODULE_ID, id, false));
-  map.clear();
-}
-
-function deregisterHooks(map) {
-  map.forEach((args, id) => {
-    const hookName = args[0];
-    Hooks.off(hookName, id);
-  });
-  map.clear();
-}
-
-function deregisterMethods(map) {
-  map.forEach((args, _id) => {
-    const cl = args[0];
-    const name = args[1];
-    delete cl[name];
-  });
-  map.clear();
-}
-
-/**
- * Register all of a given group of patches.
- */
-function registerGroup(groupName) {
-  for ( const className of Object.keys(PATCHES) ) registerGroupForClass(className, groupName);
-}
-
-/**
- * For a given group of patches, register all of them.
- */
-function registerGroupForClass(className, groupName) {
-  const grp = PATCHES[className][groupName];
-  if ( !grp ) return;
-  const {
-    WRAPS,
-    OVERRIDES,
-    METHODS,
-    GETTERS,
-    HOOKS,
-    STATIC_WRAPS,
-    STATIC_METHODS } = grp;
-  registerWraps(WRAPS, groupName, className);
-  registerWraps(OVERRIDES, groupName, className, { override: true });
-  registerWraps(STATIC_WRAPS, groupName, className, { prototype: false });
-  registerMethods(METHODS, groupName, className);
-  registerMethods(STATIC_METHODS, groupName, className, { prototype: false });
-  registerMethods(GETTERS, groupName, className, { getter: true});
-  registerHooks(HOOKS, groupName);
-}
-
-function deregisterGroup(groupName) {
-  const regObj = REG_TRACKER[groupName];
-  deregisterPatches(regObj.PATCHES);
-  deregisterMethods(regObj.METHODS);
-  deregisterHooks(regObj.HOOKS);
-}
-
-function registerWraps(wraps, groupName, className, { prototype = true, override = false } = {}) {
-  if ( !wraps ) return;
-  className = lookupByClassName(className, { returnPathString: true });
-  if ( prototype ) className = `${className}.prototype`;
-  const wrapFn = override ? "regOverride" : "regWrap";
-  for ( const [name, fn] of Object.entries(wraps) ) {
-    const methodName = `${className}.${name}`;
-    REG_TRACKER[groupName][wrapFn](methodName, fn, { perf_mode: libWrapper.PERF_FAST });
-  }
-}
-
-function registerMethods(methods, groupName, className, { prototype = true, getter = false } = {}) {
-  if ( !methods ) return;
-  let cl = lookupByClassName(className);
-  if ( prototype ) cl = cl.prototype;
-  for ( const [name, fn] of Object.entries(methods) ) {
-    REG_TRACKER[groupName].regMethod(cl, name, fn, { getter });
-  }
-}
-
-function registerHooks(hooks, groupName) {
-  if ( !hooks ) return;
-  for ( const [name, fn] of Object.entries(hooks) ) {
-    REG_TRACKER[groupName].regHook(name, fn);
-  }
-}
-
-/**
- * A thorough lookup method to locate Foundry classes by name.
- * Relies on CONFIG where possible, falling back on eval otherwise.
- * @param {string} className
- * @param {object} [opts]
- * @param {boolean} [opts.returnPathString]   Return a string path to the object, for libWrapper.
- * @returns {class}
- */
-function lookupByClassName(className, { returnPathString = false } = {}) {
-  let isDoc = className.endsWith("Document");
-  let isConfig = className.endsWith("Config");
-  let baseClass = isDoc ? className.replace("Document", "") : isConfig ? className.replace("Config", "") : className;
-
-  const configObj = CONFIG[baseClass];
-  if ( !configObj || isConfig ) return returnPathString ? className : eval?.(`"use strict";(${className})`);
-
-  // Do this the hard way to catch inconsistencies
-  switch ( className ) {
-    case "Actor":
-    case "ActiveEffect":
-    case "Item":
-      isDoc = true; break;
-  }
-
-  if ( isDoc && configObj.documentClass ) {
-    return returnPathString ? `CONFIG.${baseClass}.documentClass` : configObj.documentClass;
-  }
-
-  if ( configObj.objectClass ) return returnPathString ? `CONFIG.${baseClass}.objectClass` : configObj.objectClass;
-  return returnPathString ? className : eval?.(`"use strict";(${className})`);
-}
-
-export const REG_TRACKER = {};
-const GROUPINGS = new Set();
-export function initializeRegistrationTracker() {
-  // Determine all the relevant groupings.
-  GROUPINGS.clear();
-  Object.values(PATCHES).forEach(obj => Object.keys(obj).forEach(k => GROUPINGS.add(k)));
-
-  // Decorate each group type and create one per option.
-  for ( const key of GROUPINGS ) {
-    const regObj = REG_TRACKER[key] = {};
-    regObj.PATCHES = new Map();
-    regObj.METHODS = new Map();
-    regObj.HOOKS = new Map();
-
-    regObj.regWrap = regDec(wrap, regObj.PATCHES);
-    regObj.regOverride = regDec(override, regObj.PATCHES);
-    regObj.regMethod = regDec(addClassMethod, regObj.METHODS);
-    regObj.regHook = regDec(addHook, regObj.HOOKS);
-  }
-}
+export const PATCHER = new Patcher(PATCHES);
 
 export function initializePatching() {
-  initializeRegistrationTracker();
-  registerGroup("BASIC");
+  PATCHER.registerGroup("BASIC");
   registerPatchesForSettings();
 }
 
@@ -302,13 +91,13 @@ export function registerPatchesForSettings() {
   const visibility = getSetting(SETTINGS.TEST_VISIBILITY);
   const sweep = getSetting(SETTINGS.CLOCKWISE_SWEEP);
   unregisterPatchesForSettings();
-  if ( visibility ) registerGroup("VISIBILITY");
-  if ( sweep ) registerGroup("SWEEP");
+  if ( visibility ) PATCHER.registerGroup("VISIBILITY");
+  if ( sweep ) PATCHER.registerGroup("SWEEP");
 }
 
 function unregisterPatchesForSettings() {
-  deregisterGroup("VISIBILITY");
-  deregisterGroup("SWEEP");
+  PATCHER.deregisterGroup("VISIBILITY");
+  PATCHER.deregisterGroup("SWEEP");
 }
 
 /**
@@ -319,9 +108,9 @@ export function registerPatchesForSceneSettings() {
   const algorithm = getSceneSetting(ALGORITHM);
   unregisterPatchesForSceneSettings();
   switch ( algorithm ) {
-    case TYPES.POLYGONS: registerGroup("POLYGONS"); break;
+    case TYPES.POLYGONS: PATCHER.registerGroup("POLYGONS"); break;
     case TYPES.WEBGL: {
-      registerGroup("WEBGL");
+      PATCHER.registerGroup("WEBGL");
       canvas.effects.lightSources.forEach(src => src._initializeEVShadows());
       break;
     }
@@ -346,6 +135,6 @@ export function registerPatchesForSceneSettings() {
 }
 
 function unregisterPatchesForSceneSettings() {
-  deregisterGroup("POLYGONS");
-  deregisterGroup("WEBGL");
+  PATCHER.deregisterGroup("POLYGONS");
+  PATCHER.deregisterGroup("WEBGL");
 }
