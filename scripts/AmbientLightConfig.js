bdb405ee4e554f6edff71d5d96b9d3eb3c60e4ee
Michael Enion <caewok_2000@yahoo.com>
2024-08-29 07:39:00 -0700

    WIP|DirectionalLight|Configure directional light

--- scripts/AmbientLightConfig.js
+++ scripts/AmbientLightConfig.js
@@ -6,62 +6,71 @@ renderTemplate
 "use strict";
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 
-import { MODULE_ID, TEMPLATES, FLAGS } from "./const.js";
+import { MODULE_ID, TEMPLATES } from "./const.js";
 import { DirectionalLightSource } from "./DirectionalLightSource.js";
 import { Settings, getSceneSetting } from "./settings.js";
 
-export const PATCHES_AmbientLightConfig = {};
-export const PATCHES_TokenConfig = {};
+// Patches for the AmbientLightConfig class.
+export const PATCHES = {};
+PATCHES.BASIC = {};
 
-PATCHES_AmbientLightConfig.BASIC = {};
-PATCHES_TokenConfig.BASIC = {};
-
-async function renderTokenConfigHook(app, html, data) {
-  const template = TEMPLATES.TOKEN;
-  const findString = "div[data-tab='character']:last";
-  addTokenConfigData(app, data);
-  await injectConfiguration(app, html, data, template, findString);
+// ----- NOTE: WRAPS ----- //
+/**
+ * Wrap AmbientLightConfig#_prepareContext
+ * Add in directional light data.
+ * @param {RenderOptions} options                 Options which configure application rendering behavior
+ * @returns {Promise<ApplicationRenderContext>}   Context data for the render operation
+ * @protected
+ */
+async function _prepareContext(wrapped, options) {
+  const data = await wrapped(options);
+  calculateDirectionalData(this, data);
+  return data;
 }
-
-function addTokenConfigData(app, data) {
-  const { ALGORITHM, TYPES, LABELS } = FLAGS.ELEVATION_MEASUREMENT;
-  data.object.flags ??= {};
-  data.object.flags[MODULE_ID] ??= {};
-  data.object.flags[MODULE_ID][ALGORITHM] ??= TYPES.POINTS_CLOSE;
-
-  const renderData = {};
-  renderData[MODULE_ID] = {
-    elevationAlgorithms: LABELS
-  };
-  foundry.utils.mergeObject(data, renderData, {inplace: true});
-}
-
-PATCHES_TokenConfig.BASIC.HOOKS = {
-  renderTokenConfig: renderTokenConfigHook
-};
 
 /**
- * Inject html to add controls to the ambient light configuration to allow user to set elevation.
+ * Wrap AmbientLightConfig#._renderHTML
+ * Patch in the extra direction configs.
+ * @param {ApplicationRenderContext} context        Context data for the render operation
+ * @param {HandlebarsRenderOptions} options         Options which configure application rendering behavior
+ * @returns {Promise<Record<string, HTMLElement>>}  A single rendered HTMLElement for each requested part
  */
-async function renderAmbientLightConfigHook(app, html, data) {
+async function _renderHTML(wrapped, context, options) {
+  const rendered = await wrapped(context, options);
   const template = TEMPLATES.AMBIENT_SOURCE;
-  const findString = "div[data-tab='basic']:last";
-  calculateDirectionalData(app, data);
-  await injectConfiguration(app, html, data, template, findString);
-  activateLightConfigListeners(app, html);
+  const myHTML = await renderTemplate(template, context);
+  const div = document.createElement("div");
+  div.innerHTML = myHTML;
+
+  // Place in the basic tab at the end of the form groups
+  //const divs = context.basic.getElementsByClassName("form-group");
+  //const parent = divs[divs.length - 1].parentElement.append(div);
+  rendered.basic.append(div)
+
+  activateLightConfigListeners(this, rendered);
+  return rendered;
 }
 
-PATCHES_AmbientLightConfig.BASIC.HOOKS = {
-  renderAmbientLightConfig: renderAmbientLightConfigHook
-};
+PATCHES.BASIC.WRAPS = { _prepareContext, _renderHTML };
+
+
+/**
+ * Helper to inject configuration html into the application config.
+ */
+// async function injectConfiguration(app, html, data, template, findString) {
+//   const myHTML = await renderTemplate(template, data);
+//   const form = html.find(findString);
+//   form.append(myHTML);
+//   app.setPosition(app.position);
+// }
 
 /**
  * Calculate directional data based on position.
  */
 function calculateDirectionalData(app, data) {
-  const { x, y } = app.object;
+  const { x, y } = app.document;
   const { azimuth, elevationAngle } = DirectionalLightSource.directionalParametersFromPosition({x, y});
-  const isDirectional = Boolean(app.object.flags[MODULE_ID]?.directionalLight);
+  const isDirectional = Boolean(app.document.flags[MODULE_ID]?.directionalLight);
   const { ALGORITHM, TYPES } = Settings.KEYS.SHADING;
   const algorithm = getSceneSetting(ALGORITHM);
   const renderData = {};
@@ -72,17 +81,16 @@ function calculateDirectionalData(app, data) {
     azimuth: Math.normalizeDegrees(Math.toDegrees(azimuth)).toFixed(1),
     elevationAngle: Math.normalizeDegrees(Math.toDegrees(elevationAngle)).toFixed(1),
     isDirectional };
-  foundry.utils.mergeObject(data.data, renderData, {inplace: true});
+  foundry.utils.mergeObject(data, renderData, {inplace: true});
 }
-
 
 /**
  * Catch when the user selects directional wall to update the submenu options.
  */
-function activateLightConfigListeners(app, html) {
-  html.on("click", "#elevatedvisionDirectionalLightCheckbox", onCheckDirectional.bind(app));
-  html.on("change", "#elevatedvisionAzimuthConfig", onChangeAzimuth.bind(app));
-  html.on("change", "#elevatedvisionElevationAngleConfig", onChangeElevationAngle.bind(app));
+function activateLightConfigListeners(app, rendered) {
+  rendered.basic.addEventListener("click", onCheckDirectional.bind(app));
+  rendered.basic.addEventListener("change", onChangeAzimuth.bind(app));
+  rendered.basic.addEventListener("change", onChangeElevationAngle.bind(app));
 }
 
 /**
@@ -90,7 +98,7 @@ function activateLightConfigListeners(app, html) {
  */
 function onChangeAzimuth(event) {
   const azimuth = Math.toRadians(Number(event.target.value));
-  const clone = this.object.object._preview;
+  const clone = this.document.object._preview;
   if ( !clone ) return;
 
   const { x, y} = DirectionalLightSource.positionFromDirectionalParameters(azimuth, clone.source.elevationAngle);
@@ -103,7 +111,7 @@ function onChangeAzimuth(event) {
  */
 function onChangeElevationAngle(event) {
   const elevationAngle = Math.toRadians(Number(event.target.value));
-  const clone = this.object.object._preview;
+  const clone = this.document.object._preview;
   if ( !clone ) return;
 
   const { x, y } = DirectionalLightSource.positionFromDirectionalParameters(clone.source.azimuth, elevationAngle);
@@ -115,35 +123,22 @@ function onChangeElevationAngle(event) {
  * Update submenu visibility
  */
 function onCheckDirectional(event) {
-  const elemElevation = document.getElementById("elevatedvision-config-elevation");
   const elemAzimuth = document.getElementById("elevatedvision-config-azimuth");
   const elemElevationAngle = document.getElementById("elevatedvision-config-elevationAngle");
   const elemSolarAngle = document.getElementById("elevatedvision-config-solarAngle");
-  const clone = this.object.object._preview;
+  const clone = this.document.object._preview;
   const directionalLightChecked = event.target.checked;
 
   if ( directionalLightChecked ) {
     if ( clone ) clone.convertToDirectionalLight();
-    elemElevation.style.display = "none";
     elemAzimuth.style.display = "block";
     elemElevationAngle.style.display = "block";
     elemSolarAngle.style.display = "block";
 
   } else {  // Point source
     if ( clone ) clone.convertFromDirectionalLight();
-    elemElevation.style.display = "block";
     elemAzimuth.style.display = "none";
     elemElevationAngle.style.display = "none";
     elemSolarAngle.style.display = "none";
   }
-}
-
-/**
- * Helper to inject configuration html into the application config.
- */
-async function injectConfiguration(app, html, data, template, findString) {
-  const myHTML = await renderTemplate(template, data);
-  const form = html.find(findString);
-  form.append(myHTML);
-  app.setPosition(app.position);
 }
