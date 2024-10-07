6b3004b2a96424d4661b649f17cc67b7b0d93382
Michael Enion <caewok_2000@yahoo.com>
2024-10-07 08:24:03 +0200

    💡 refactor|Controls|Remove subcontrols

--- scripts/controls.js
+++ scripts/controls.js
@@ -7,34 +7,14 @@ Hooks
 */
 "use strict";
 
-/*
-Anticipated UI:
-
-Controls:
-- Current elevation for painting. Scroll wheel to increment / decrement
-- Tool to fill by grid square
-- Tool to fill all space contained between walls
-- Tool to fill by pixel. Resize and choose shape: grid square, hex, circle. Reset size to grid size.
-- Reset
-- Undo
-*/
-
 import { ElevationLayerToolBar } from "./ElevationLayerToolBar.js";
 import { MODULE_ID } from "./const.js";
 
 Hooks.on("getSceneControlButtons", addElevationLayerSceneControls);
-Hooks.on("renderSceneControls", (controls) => {
-  addElevationLayerSubControls(controls)
-  drawBrush(controls);
-});
-Hooks.on("renderTerrainLayerToolBar", renderElevationLayerSubControls);
 Hooks.on("getSceneControlButtons", addDirectionalLightingControl);
 
 function addDirectionalLightingControl(controls) {
-  // const isGM = game.user.isGM;
   const lighting = controls.find(c => c.name === "lighting");
-  // if ( lighting.tools.some(t => t.name === "directionalLight") ) return;
-
   const directionalTool = {
     name: "directional-light",
     title: game.i18n.localize(`${MODULE_ID}.controls.directional-light.name`),
@@ -70,37 +50,4 @@ function addElevationLayerSceneControls(controls) {
       }
     ]
   });
-}
-
-function addElevationLayerSubControls(controls) {
-  if ( !canvas || !canvas.elevation ) return;
-
-  if ( controls.activeControl === "elevation" ) {
-    if ( !canvas.elevation.toolbar ) canvas.elevation.toolbar = new ElevationLayerToolBar();
-    canvas.elevation.toolbar.render(true);
-
-  } else {
-    if ( !canvas.elevation.toolbar ) return;
-    canvas.elevation.toolbar.close();
-  }
-}
-
-function drawBrush(controls) {
-  if ( !canvas.elevation ) return;
-  switch (controls.tool) {
-    case 'fill-by-pixel':
-      canvas.elevation.drawBrush();
-      break;
-    default:
-      break;
-  }
-}
-
-function renderElevationLayerSubControls() {
-  if ( !canvas.elevation.toolbar ) return;
-  const tools = $(canvas.elevation.toolbar.form).parent();
-  if ( !tools ) return;
-  const controltools = $("li[data-tool='fill-by-pixel']").closest(".sub-controls");
-  controltools.addClass("elevation-controls");
-  canvas.elevation.toolbar.element.addClass("active");
 }
