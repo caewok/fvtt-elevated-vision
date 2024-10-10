f16806e5b128032d575da934f67da8e8d258337b
Michael Enion <caewok_2000@yahoo.com>
2024-10-10 20:58:13 +0200

    🎸 feat |LightWall|Change light shape when light size is set
    Set the shape to match the light size

--- scripts/PointLightSource.js
+++ scripts/PointLightSource.js
@@ -1,5 +1,6 @@
 /* globals
-
+LimitedAnglePolygon,
+PIXI
 */
 "use strict";
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
@@ -31,9 +32,46 @@ function _initialize(wrapped, data) {
  * See issue #77.
  */
 function _getPolygonConfiguration(wrapped) {
-  const config = wrapped();
-  if ( Settings.get(Settings.KEYS.LIGHTS_FULL_PENUMBRA) ) config.type = "universal";
-  return config;
+  const cfg = wrapped();
+  if ( Settings.get(Settings.KEYS.LIGHTS_FULL_PENUMBRA) ) cfg.type = "universal";
+
+  // When the point source width is larger than 0 and there is an angle,
+  // add a boundary shape that allows for a flat space at the origin.
+  // So a trapezoid narrowly focused at the end.
+  if ( cfg.angle === 0 || cfg.angle === 360 || cfg.angle === 180 || this.isDirectional ) return cfg;
+
+  const lightSize = this.object.document.getFlag(MODULE_ID, FLAGS.LIGHT_SIZE);
+  if ( !lightSize ) return cfg;
+
+  // Instead of a limited angle, use a trapezoid shape.
+  const la = new LimitedAnglePolygon(this.data, { angle: cfg.angle, rotation: cfg.rotation, radius: cfg.radius });
+
+  const rotRad = Math.toRadians(cfg.rotation);
+  const a = PIXI.Point.fromAngle(this.data, rotRad, lightSize);
+  const b = PIXI.Point.fromAngle(this.data, rotRad, -lightSize);
+  const c = a.fromAngle(la.aMin, cfg.radius);
+  const d = b.fromAngle(la.aMax, cfg.radius);
+
+  // Construct polygon points for the primary angle
+  // Extend out the limited angle polygon to reach the radius at c and d.
+  const extRadius = PIXI.Point.distanceBetween(this.data, c);
+  const { x, y } = la.origin;
+  const primaryAngle = la.aMax - la.aMin;
+  const nPrimary = Math.ceil((primaryAngle * la.density) / (2 * Math.PI));
+  const dPrimary = primaryAngle / nPrimary;
+  const points = [];
+  const o = PIXI.Point.fromObject(la.origin);
+  for ( let i = 0; i <= nPrimary; i++ ) {
+    const pad = o.fromAngle(la.aMin + (i * dPrimary), extRadius, PIXI.Point._tmp);
+    points.push(pad.x, pad.y);
+  }
+
+  const trapezoid = new PIXI.Polygon(a.x, a.y, c.x, c.y, ...points, d.x, d.y, b.x, b.y);
+  cfg.angle = 360;
+  cfg.radius = extRadius;
+  cfg.boundaryShapes ??= [];
+  cfg.boundaryShapes.push(trapezoid);
+  return cfg;
 }
 
 /**
