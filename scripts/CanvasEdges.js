440e4d832b241b841e14d0e8236dc94608b635fc
Michael Enion <caewok_2000@yahoo.com>
2025-01-31 09:24:09 -0800

    💍 test|BVH|Add hook to create edge texture

---
+++ scripts/CanvasEdges.js
@@ -0,0 +1,43 @@
+/* globals
+canvas,
+Wall
+*/
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+"use strict";
+
+import { MODULE_ID } from "./const.js";
+import { EdgeData } from "./glsl/BVH.js";
+import { WebGLShadowsBVH } from "./glsl/WebGLShadowsBVH.js";
+import { PixelCache } from "./geometry/PixelCache.js";
+
+// Track wall creation, update, and deletion, constructing WallTracerEdges as we go.
+// Use to update the pathfinding triangulation.
+
+export const PATCHES = {};
+PATCHES.BASIC = {};
+
+// ----- NOTE: Hooks ----- //
+
+/**
+ * Hook initializeEdges
+ * Set up the SCENE GRAPH with all wall edges.
+ */
+function initializeEdges() {
+  if ( CONFIG[MODULE_ID].webGLShadowClass !== WebGLShadowsBVH ) return;
+  const t0 = performance.now();
+
+  // See ElevationTextureHandler.js.
+  CONFIG[MODULE_ID].edgeData = EdgeData.edges.map(edge => new EdgeData(edge));
+  const config = EdgeData.textureConfiguration();
+  CONFIG[MODULE_ID].edgeTexture = PIXI.RenderTexture.create(config);
+  CONFIG[MODULE_ID].edgeCache = PixelCache.fromTexture(
+    CONFIG[MODULE_ID].edgeTexture,
+    { arrayClass: Uint16Array }
+  );
+  EdgeData.copyEdgesToArray(CONFIG[MODULE_ID].edgeCache.pixels)
+
+  const t1 = performance.now();
+  console.debug(`${MODULE_ID}|Created edge cache in ${t1 - t0} ms.`);
+}
+
+PATCHES.BASIC.HOOKS = { initializeEdges };
